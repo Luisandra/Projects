f4238502168e56b339d898ff9e0d5116c84f0a23
Luisandra <LuisandraMM@gmail.com>
2020-03-05 16:10:08 -0600

    JS: delete and complete functional

--- ToDoListApp/Assets/JS/toDoScript.js
+++ ToDoListApp/Assets/JS/toDoScript.js
@@ -1,1 +1,27 @@
-alert("connected!");
+// Strike through for completed list item 
+// CSS class created and applied/removed 
+$("li").on("click", function(){
+    $(this).toggleClass("completed");
+})
+
+
+//Removing list items when clicking delete
+$(".delete").on("click", function(event){
+    $(this).parent().fadeOut(1000, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+//    MY CODE - Over-complicated
+//     var listItem = $(this).closest("li");
+//     var timer = 5000;
+//     $(this).fadeOut(timer, function(){
+//         // listItem.remove();
+//         $(this).remove();
+//     });
+//     listItem.fadeOut(timer, function(){
+//         listItem.remove();
+//         // $(this).remove();
+//     });
+    // Will stop Li listener from firing
+    
+})
